687fe8895428b4d643dab058f6317a27280d9907
Ctrldanii <daniellaa412@stemcollegiate.ca>
2025-02-18 12:23:38 -0700

    Create Feb18.cs

---
+++ Feb18.cs
@@ -0,0 +1,10 @@
+// Today I learnt how to declare some variable like the char variable
+string firstName;
+firstName = "Daniella";
+Console.WriteLine(firstName);
+firstName = "Harini";
+Console.WriteLine(firstName);
+firstName = "Avneet";
+Console.WriteLine(firstName);
+firstName = "Helene";
+Console.WriteLine(firstName);
