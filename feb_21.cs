e6d147378bf0d48a277fdf777ddb548e8c1f137b
Ctrldanii <daniellaa412@stemcollegiate.ca>
2025-02-21 08:54:51 -0700

    Update feb_21.cs

--- feb_21.cs
+++ feb_21.cs
@@ -1,1 +1,5 @@
-
+// I made a sentence using "int" "string" and "double"
+string firstName = "Daniella";
+int age = 15;
+double temp = 8.0;
+Console.WriteLine($"Hello, {firstName}! You turned {age} on the 3rd of February. The temperature was {temp} degrees that day ");
