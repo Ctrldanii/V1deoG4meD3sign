bd18581d5c84c580476823da73273020ad8f6f99
Ctrldanii <daniellaa412@stemcollegiate.ca>
2025-05-06 12:27:46 -0600

    Create May6.cs

---
+++ May6.cs
@@ -0,0 +1,2 @@
+//My game is solitaire. It requires intricate attention and critical thinking in order to complete the game. This sharpens your thinking skills as well as your problmem solving skills.
+//You can play it alone as a way to pass time or you can play it with friends and have fun. I would reccomend this game because it is educational and fun
